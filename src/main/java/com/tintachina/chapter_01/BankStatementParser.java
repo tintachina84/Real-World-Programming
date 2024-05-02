434fd215783ce04107230802367f8db980fddcae
seonghyeon.seo <tintachina84@gmail.com>
2024-05-02 13:29:40 +0900

    Added: Chapter 2.

---
+++ src/main/java/com/tintachina/chapter_01/BankStatementParser.java
@@ -0,0 +1,8 @@
+package com.tintachina.chapter_01;
+
+import java.util.List;
+
+public interface BankStatementParser {
+    BankTransaction parseFrom(String line);
+    List<BankTransaction> parseLinesFrom(List<String> lines);
+}
---
+++ src/main/java/com/tintachina/chapter_01/MainApplication.java
@@ -0,0 +1,15 @@
+package com.tintachina.chapter_01;
+
+public class MainApplication {
+    public static void main(String[] args) throws Exception {
+
+        final BankStatementAnalyzer bankStatementAnalyzer
+                = new BankStatementAnalyzer();
+
+        final BankStatementParser bankStatementParser
+                = new BankStatementCSVParser();
+
+        bankStatementAnalyzer.analyze("bank-data-simple.csv", bankStatementParser);
+
+    }
+}
