ee56cf2fcc14a7680761ce65a9a4ca5e0115a852
seonghyeon.seo <tintachina84@gmail.com>
2024-05-09 10:22:51 +0900

    Fixed: formatting google java code style.

--- src/main/java/com/tintachina/chapter_03/Document.java
+++ src/main/java/com/tintachina/chapter_03/Document.java
@@ -4,14 +4,15 @@ import java.util.Map;
 
 // tag::document[]
 public class Document {
-    private final Map<String, String> attributes;
 
-    Document(final Map<String, String> attributes) {
-        this.attributes = attributes;
-    }
+  private final Map<String, String> attributes;
 
-    public String getAttribute(final String attributeName) {
-        return attributes.get(attributeName);
-    }
+  Document(final Map<String, String> attributes) {
+    this.attributes = attributes;
+  }
+
+  public String getAttribute(final String attributeName) {
+    return attributes.get(attributeName);
+  }
 }
 // end::document[]
--- src/main/java/com/tintachina/chapter_03/UnknownFileTypeException.java
+++ src/main/java/com/tintachina/chapter_03/UnknownFileTypeException.java
@@ -1,7 +1,8 @@
 package com.tintachina.chapter_03;
 
 public class UnknownFileTypeException extends RuntimeException {
-    public UnknownFileTypeException(final String message) {
-        super(message);
-    }
+
+  public UnknownFileTypeException(final String message) {
+    super(message);
+  }
 }
